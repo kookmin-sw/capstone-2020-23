64d01747e233afc284f072ca4fced5cb0e909b9b
20162819 <56514477+20162819@users.noreply.github.com>
2020-04-09 03:19:30 +0900

    add drawerlayout, custom app-bar base

--- MoaYo/app/src/main/java/com/capstone/moayo/MainActivity.java
+++ MoaYo/app/src/main/java/com/capstone/moayo/MainActivity.java
@@ -6,12 +6,15 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.widget.ToolbarWidgetWrapper;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -23,13 +26,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //리소스 파일에서 추가한 툴바를 앱바로 지정하기
-        Toolbar mainToolBar = (Toolbar) findViewById(R.id.mainToolBar);
-        setSupportActionBar(mainToolBar);
-
-        getSupportActionBar().setTitle(""); //앱바에서 제목을 없애고 activity_main.xml에서 설정한 제목이 뜨게 설정
+//        //리소스 파일에서 추가한 툴바를 앱바로 지정하기
+//        Toolbar mainToolBar = (Toolbar) findViewById(R.id.mainToolBar);
+//        setSupportActionBar(mainToolBar);
+//
+//        getSupportActionBar().setTitle(""); //앱바에서 제목을 없애고 activity_main.xml에서 설정한 제목이 뜨게 설정
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
 
         Button temp = (Button) findViewById(R.id.tempButton);
 
@@ -46,36 +50,39 @@ public class MainActivity extends AppCompatActivity {
     //mainToolBar에 menu.xml을 인플레이트함
 
     public boolean onCreateOptionsMenu(Menu menu) {
-        //return super.onCreateOptionsMenu(menu);
+
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu, menu);
         return true;
     }
 
-    //추가된 소스, ToolBar에 추가된 항목의 select 이벤트를 처리하는 함수
 
     public boolean onOptionsItemSelected(MenuItem item) {
 
 
-        //return super.onOptionsItemSelected(item);
+        //menu.xml에서 지정한 item 이벤트 추가
         switch (item.getItemId()) {
+
+
+            default:
+            {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.mainDisplay);
+                drawer.openDrawer(Gravity.LEFT);
+
+                return true;
+            }
 
             case R.id.moveMY:
                 // User chose the "Settings" item, show the app settings UI
                 Toast.makeText(getApplicationContext(), "나의 도감으로 이동함", Toast.LENGTH_LONG).show();
                 return true;
 
-            default: {
-
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                Toast.makeText(getApplicationContext(), "나머지 버튼 클릭됨", Toast.LENGTH_LONG).show();
-
-                return super.onOptionsItemSelected(item);
-            }
+//            default:
+//                Toast.makeText(getApplicationContext(),"메뉴 Tab 펼쳐짐", Toast.LENGTH_LONG).show();
+//                return true;
 
         }
+
     }
-
-
 }
+
--- MoaYo/app/src/main/java/com/capstone/moayo/ResultActivity.java
+++ MoaYo/app/src/main/java/com/capstone/moayo/ResultActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -14,30 +19,66 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        //사용자 도감 탭 펼치기 전, 탭 화면 객체 참조
-        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.resultDisplay);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
 
-        //사용자 도감 탭 화면 객체 참조
-        final View drawerView = (View) findViewById(R.id.categoryList);
+//        //사용자 도감 탭 펼치기 전, 탭 화면 객체 참조
+//        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.resultDisplay);
+//
+//        //사용자 도감 탭 화면 객체 참조
+//        final View drawerView = (View) findViewById(R.id.categoryList);
+//
+//        //도감 리스트 화면을 열고 닫을 버튼 객체 참조
+//        Button btn_categoryOpen = (Button) findViewById(R.id.categoryOpen);
+//        Button btn_categoryClose = (Button) findViewById(R.id.categoryClose);
+//
+//        //도감 리스트 화면 여는 버튼리스너
+//        btn_categoryOpen.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                drawerLayout.openDrawer(drawerView);
+//            }
+//        });
+//
+//        //도감 리스트 화면 닫는 버튼리스너
+//        btn_categoryClose.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                drawerLayout.closeDrawer(drawerView);
+//            }
+//        });
+    }
 
-        //도감 리스트 화면을 열고 닫을 버튼 객체 참조
-        Button btn_categoryOpen = (Button) findViewById(R.id.categoryOpen);
-        Button btn_categoryClose = (Button) findViewById(R.id.categoryClose);
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-        //도감 리스트 화면 여는 버튼리스너
-        btn_categoryOpen.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                drawerLayout.openDrawer(drawerView);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_result, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        //menu.xml에서 지정한 item 이벤트 추가
+        switch (item.getItemId()) {
+
+
+            case R.id.myCate:
+            {
+                //사용자 도감 탭 펼치기 전, 탭 화면 객체 참조
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.resultDisplay);
+                drawer.openDrawer(Gravity.RIGHT);
+
+                return true;
             }
-        });
+            default:
+                onBackPressed();
+                return true;
+//            default:
+//                Toast.makeText(getApplicationContext(),"메뉴 Tab 펼쳐짐", Toast.LENGTH_LONG).show();
+//                return true;
 
-        //도감 리스트 화면 닫는 버튼리스너
-        btn_categoryClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawerLayout.closeDrawer(drawerView);
-            }
-        });
+        }
+
     }
 }
