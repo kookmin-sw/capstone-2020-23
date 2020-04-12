42d146fadeed0dd137790dfd147d4fe319906625
gilwoong <smostr@kookmin.ac.kr>
2020-04-12 23:39:41 +0900

    dao 테스트 완료.

--- MoaYo/app/src/main/java/com/capstone/moayo/dao/CategoryDao.java
+++ MoaYo/app/src/main/java/com/capstone/moayo/dao/CategoryDao.java
@@ -1,4 +1,17 @@
 package com.capstone.moayo.dao;
 
+import android.database.Cursor;
+
+import com.capstone.moayo.dao.sqlite.DBHelper;
+
 public interface CategoryDao {
+    public long insert(DBHelper dbHelper,int level, int parent, String title);
+
+    public boolean update(DBHelper dbHelper,int id,int level,int parent, String title);
+
+    public boolean delete(DBHelper dbHelper,int id);
+    // 커서 이용한 뒤 close()할것.
+    public Cursor select(DBHelper dbHelper, int id);
+
+    public Cursor selectAll(DBHelper dbHelper);
 }
