92fb5fcc84ded3c41ee6eafe007d4f7df3440979
gilwoong <smostr@kookmin.ac.kr>
2020-04-18 15:44:03 +0900

    연관 태그 추적

--- MoaYo/app/src/main/java/com/capstone/moayo/MainActivity.java
+++ MoaYo/app/src/main/java/com/capstone/moayo/MainActivity.java
@@ -2,7 +2,9 @@ package com.capstone.moayo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -27,7 +29,12 @@ import com.capstone.moayo.util.Exception.NotRootException;
 import com.capstone.moayo.storage.StorageFactory;
 import com.capstone.moayo.storage.concrete.StorageFactoryCreator;
 import com.capstone.moayo.util.Exception.DaoObjectNullException;
+import com.capstone.moayo.util.Tag.RequestHttpConnection;
 import com.google.gson.Gson;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class MainActivity extends AppCompatActivity {
     private Button createBtn;
@@ -35,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private Button DBButton;
     private Button findBtn;
     private Button deleteBtn;
+    private Button getTagBtn;
 
     private CategoryService categoryService;
     private DataBindingService dataBindingService;
@@ -55,8 +63,45 @@ public class MainActivity extends AppCompatActivity {
         Button DBDel = findViewById(R.id.DBdel);
         findBtn = findViewById(R.id.findBtn);
         deleteBtn = findViewById(R.id.deleteBtn);
+        getTagBtn = findViewById(R.id.GetTag);
 
         DBHelper mDBHelper = storageFactory.initDao(this);
+
+        getTagBtn.setOnClickListener(v->{
+            // OOTD 대신 다른 태그 넣어서 확인해봐. 아직 한글은 안해봄, print로 출력하니 run에서 확인할 것.
+            String url = "https://www.tagsfinder.com/ko-kr/related/OOTD";
+            class NetworkTask extends AsyncTask<Void, Void, String> {
+
+                private String url;
+                private ContentValues values;
+
+                public NetworkTask(String url, ContentValues values) {
+
+                    this.url = url;
+                    this.values = values;
+                }
+
+                @Override
+                protected String doInBackground(Void... params) {
+
+                    String result; // 요청 결과를 저장할 변수.
+                    RequestHttpConnection requestHttpURLConnection = new RequestHttpConnection();
+                    result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+                    return result;
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+                    Document document = Jsoup.parse(s);
+                    Element result = document.getElementById("hashtagy");
+                    System.out.println(result);
+                }
+            }
+            NetworkTask networkTask = new NetworkTask(url,null);
+            networkTask.execute();
+        });
 
         DBDel.setOnClickListener(v->{
             try {
