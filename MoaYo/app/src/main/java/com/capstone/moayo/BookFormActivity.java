4c8bffcbd8817eb35c982a510a86178f31b2ac92
labiss96 <cxz9080@gmail.com>
2020-05-07 20:58:37 +0900

    Create form Fragment & Apply transition animation.

--- MoaYo/app/src/main/java/com/capstone/moayo/BookFormActivity.java
+++ MoaYo/app/src/main/java/com/capstone/moayo/BookFormActivity.java
@@ -1,18 +1,16 @@
 package com.capstone.moayo;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class BookFormActivity extends AppCompatActivity{
+public class BookFormActivity extends AppCompatActivity {
 //    FragmentManager fm;
 //    FragmentTransaction tran;
 //    FormFragment firstFrag;
-    Button start_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +20,31 @@ public class BookFormActivity extends AppCompatActivity{
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("도감 생성");
 
-        start_btn = (Button) findViewById(R.id.start_form_btn);
-
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.form_frame, new FormFragment());
-        transaction.commit();
 
+//        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left );
+        transaction.replace(R.id.form_frame, new FormMainFragment()).commit();
+    }
+
+    //프래그먼트와 프래그먼트끼리 직접접근을하지않는다. 프래그먼트와 엑티비티가 접근함
+    public void onFragmentChange(int index) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left);
+        fragmentTransaction.addToBackStack(null);
+
+        switch (index) {
+            case 0:
+                fragmentTransaction.replace(R.id.form_frame, new FormMainFragment()).commit();
+                break;
+            case 1:
+                fragmentTransaction.replace(R.id.form_frame, new FormEditFragment()).commit();
+                break;
+            default:
+                fragmentTransaction.replace(R.id.form_frame, new FormMainFragment()).commit();
+                break;
+        }
     }
 //
 //    @Override
--- MoaYo/app/src/main/java/com/capstone/moayo/FormMainFragment.java
+++ MoaYo/app/src/main/java/com/capstone/moayo/FormMainFragment.java
@@ -4,23 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FormFragment extends Fragment {
+public class FormMainFragment extends Fragment {
 
     View view;
+    Button start_btn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.form_fragment_first, container, false);
+        view = inflater.inflate(R.layout.fragment_form_main, container, false);
+
+        start_btn = (Button) view.findViewById(R.id.start_form_btn);
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((BookFormActivity)getActivity()).onFragmentChange(1);
+            }
+        });
 
         return view;
     }
