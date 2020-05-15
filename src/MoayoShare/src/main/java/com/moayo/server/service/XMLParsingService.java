1e9439532d79a828c42630551419aaf5140d552c
JeongJunKwon <37372602+JeongJunKwon@users.noreply.github.com>
2020-05-15 17:08:22 +0900

    Revert "Merge branch 'shareServer_develop' into develop_backend_v2"
    This reverts commit 08b291c52691fbaa5c3fe5c4857dca8390a20a11, reversing
    changes made to b7525ac108bcd291bdc30e021ba8d658f3c2eb2b.

--- src/MoayoShare/src/main/java/com/moayo/server/service/XMLParsingService.java
+++
@@ -1,8 +0,0 @@
-package com.moayo.server.service;
-
-import com.moayo.server.model.DogamListModel;
-import org.w3c.dom.Document;
-
-public interface XMLParsingService {
-    public DogamListModel insertData(Document doc);
-}
