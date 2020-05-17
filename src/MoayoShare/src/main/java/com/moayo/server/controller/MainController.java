4f16cced3b734659bbd9a68ac2a89e0e5536f27a
gilwoong <smostr@kookmin.ac.kr>
2020-05-17 16:25:30 +0900

    Revert "도감 내보내기, 도감 리스트 내보내기, 도감 공유하기 기능 구현"
    This reverts commit 09d77a1727499094341c7cf9ac91ae14b9649e2a.

--- src/MoayoShare/src/main/java/com/moayo/server/controller/MainController.java
+++ src/MoayoShare/src/main/java/com/moayo/server/controller/MainController.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 public class MainController {
@@ -43,8 +42,8 @@ public class MainController {
         jsonParsingService.insertData(dogamModel);
         return "0000";
     }
-    @RequestMapping(value = "/getDogamList",method = RequestMethod.GET)
-    public List<DogamListModel> getDogamList(@RequestParam String hashtag){
-        return service.getDogamList();
-    }
+//    @RequestMapping(value = "/getDogamList",method = RequestMethod.POST)
+//    public DogamListModel[] getDogamList(@RequestParam String hashtag){
+//
+//    }
 }
