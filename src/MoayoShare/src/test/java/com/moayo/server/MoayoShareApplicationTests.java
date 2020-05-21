a208e4ab78fee7740bb0df038e02b9bfa76fdf9b
labiss96 <cxz9080@gmail.com>
2020-05-21 14:47:20 +0900

    restore loss code of instacrawler, MoayoShare.

---
+++ src/MoayoShare/src/test/java/com/moayo/server/MoayoShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moayo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoayoShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
