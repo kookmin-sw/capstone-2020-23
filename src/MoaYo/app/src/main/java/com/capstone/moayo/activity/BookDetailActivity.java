2d1af315df1db0d578072a7ec7a00b84ad5a3f0f
20162819 <56514477+20162819@users.noreply.github.com>
2020-05-28 02:16:56 +0900

    delete unused code

--- src/MoaYo/app/src/main/java/com/capstone/moayo/activity/BookDetailActivity.java
+++ src/MoaYo/app/src/main/java/com/capstone/moayo/activity/BookDetailActivity.java
@@ -1,18 +1,17 @@
 package com.capstone.moayo.activity;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.widget.Adapter;
-import android.widget.ExpandableListAdapter;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.TextView;
-//import android.widget.ExpandableListView.OnChildClickListener;
-//import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.capstone.moayo.BaseActivity;
